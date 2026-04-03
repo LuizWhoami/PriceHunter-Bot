8cf2e95cb8f867cefef62a24019896b9bb746a20
LuizWhoami <binckplink@gmail.com>
2026-04-03 19:18:13 -0300

    quinto já

---
+++ explicando_abocois.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.python.org/blogs/'
+resposta = requests.get('url')
+
+if resposta.status_code == 200:
+    print('conexão feita')
+
+    soup = BeautifulSoup(resposta.content, 'html.parser')
--- main.py
+++ main.py
@@ -1,64 +1,96 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep
 
-def enviar():
+
+url = 'https://www.mercadolivre.com.br/ofertas?container_id=MLB779539-1&domain_id=MLB-TELEVISIONS#filter_applied=domain_id&filter_position=12&is_recommended_domain=false&origin=scut'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print('conexão com sucesso')
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    pega_nome = soup.find_all('div', class_='poly-card__content')
     
-    chat_id = '-5102848021'
-    token = '8788032847:AAEOwVrWD-Y017Qk4Mewm6VSF8Yh9JllLNE'
+    resultados = []
+    
+    for itens in pega_nome:
 
+        produto = itens.find('h3')
+        preco = itens.find('span', class_='andes-money-amount')
+        desconto = itens.find('span', class_='poly-price__disc_label')
+
+        if produto and preco and desconto:
+
+            produto1 = produto.text.strip()
+            preco1 = preco.text.strip()
+            desconto1 = desconto.text.strip()   
+
+            tag_link = itens.find('a')
+            link = tag_link['href'] if tag_link else "Link não encontrado"
+            linkis = link.strip()
+
+            dados = {
+                'Produto': [produto1],
+                'Preço': [preco1],
+                'Desconto': [desconto1],
+                'Link': [linkis]
+            }
+            resultados.append(dados)
+
+        if produto:
+            print(f'Produto: {produto1}')
+            print(f'Preço:  {preco1} ')
+            if desconto1:   
+                print(f'Desconto: {desconto1}')
+            else:
+                print('Sem desconto')
+            print(f'Link: {linkis} \n \n')
+        else:
+            print(f'Ocorreu um erro {produto and preco and desconto}')
+
+
+df = pd.DataFrame(resultados)
+df.to_excel('Produtos.xlsx', index=False, engine='openpyxl')
+print('Dados salvos')
+
+chat_id = '-5102848021'
+token = '8788032847:AAEOwVrWD-Y017Qk4Mewm6VSF8Yh9JllLNE'
+
+for index, linha in df.iterrows():
+    produto2 = linha['Produto']
+    preco2 = linha['Preço']
+    desconto2 = linha['Desconto']
+    link2 = linha['Link']
+
+    mensagem = (
+        f"{'-'*100} \n"
+        f"📦 Produto: {produto2} \n \n"
+        f"💰 Preço: {preco2} \n \n"
+        f"📉 Desconto: {desconto2} \n \n"
+        f"🔗 Link: {link2} \n"
+        f"{'-'*100}"
+    )
+    
     url_telegram = f'https://api.telegram.org/bot{token}/sendMessage'
-
     payload = {
         'chat_id': chat_id,
         'token': token,
-        "parse_mode": "Markdown",
-        'text': f'{produto1} \n {preco1} \n {desconto1}'
+        'text': mensagem,
+        'disable_web_page_preview': False
     }
 
     resposta = requests.post(url_telegram, payload)
 
     if resposta.status_code == 200:
         print('Mensagem enviada com sucesso')
+        sleep(2)
 
     else:
         print(f'Erro {resposta.status_code}')
         print(resposta.text)
-
-def mercado():
-    global produto1, preco1, desconto1
-    url = 'https://lista.mercadolivre.com.br/pc'
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        print('conexão com sucesso')
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        pega_nome = soup.find_all('div', class_='poly-card__content')
-        
-        for itens in pega_nome:
-
-            produto = itens.find('h3')
-            preco = itens.find('span', class_='andes-money-amount')
-            desconto = itens.find('span', class_='andes-money-amount__discount')
-
-            produto1 = produto.text.strip()
-            preco1 = preco.text.strip()
-            desconto1 = desconto.text.strip()
-
-            tag_link = itens.find('a')
-            link = tag_link['href'] if tag_link else "Link não encontrado"
-            if produto:
-                print(f'Produto: {produto1}')
-                print(f'Preço:  {preco1} ')
-                if desconto:   
-                    print(f'Desconto: {desconto1}')
-                else:
-                    print('Sem desconto')
-                print(f'Link: {link} \n \n')
-
-enviar(mercado())
-
-print(f'Mensgem enviada: \n {produto1}')
